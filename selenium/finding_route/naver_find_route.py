c2792ab815014977a7833013d8618444f4bb1820
skytreesea <skytreesea@gmail.com>
2020-12-21 07:52:46 +0900

    Update naver_find_route.py

--- selenium/finding_route/naver_find_route.py
+++ selenium/finding_route/naver_find_route.py
@@ -8,8 +8,8 @@ from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome(r'C:\Users\ERC\Documents\파이썬\chromedriver.exe')
 driver.get('https://map.naver.com/v5/directions/-/-/-/transit?c=14139268.8290314,4507639.6434387,15,0,0,0,dh')
 # 시작하는 지점과 끝을 입력하세요. 
-start = '한국교원대'
-finish = '국토연구원'
+start = '경복궁'
+finish = '낙성대'
 # 불러오는 시간이 있으니 약간의 딜레이를 줍니다. 
 time.sleep(3)
 # 자동차 거리를 선택합니다. 
@@ -25,11 +25,10 @@ el = driver.find_element_by_id('directionGoal1')
 time.sleep(1)
 el.send_keys(finish)
 el.send_keys(Keys.RETURN)
-time.sleep(1)
-el.send_keys(Keys.RETURN)
-time.sleep(2)
+time.sleep(3)
 # 루트 정보를 찾아 입력하도록 합니다.
-group = driver.find_elements_by_class_name('route_title') 
+group = driver.find_elements_by_class_name('btn btn_direction active') 
+el.send_keys(Keys.RETURN)
 time.sleep(2)
 for i in group:
     print(i.text)
