9b6da9ef6ffd9cd0a7072f710f38669b7c9148ff
skytreesea <skytreesea@gmail.com>
2020-12-20 14:33:27 +0900

    Change the input of path
    경로 입력을 바꿈

--- basic_scaping/stock/img/image_basic_suc.py
+++ basic_scaping/stock/img/image_basic_suc.py
@@ -2,7 +2,7 @@ import requests, os
 image_url = 'https://ssl.pstatic.net/imgfinance/chart/item/area/day/195990.png?sidcode=1608439379118'
 #image_url으로부터 content 얻음
 img_data = requests.get(image_url).content
-os.chdir(r'C:\Users\ERC\Documents\GitHub\fast_campus\basic_scaping\stock')
+os.chdir(r'####경로를 정확히 입력 #####')
 #content 저장함
 with open('image_name.jpg', 'wb') as handler:
     handler.write(img_data)
