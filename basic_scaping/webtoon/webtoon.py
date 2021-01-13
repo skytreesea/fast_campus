bf0f2164c5f31f468949f3329e600170a3548d94
skytreesea <skytreesea@gmail.com>
2021-01-13 09:04:04 +0900

    scrape the posts of brunch for understanding the principle of urls

--- basic_scaping/webtoon/webtoon.py
+++ basic_scaping/webtoon/webtoon.py
@@ -1,21 +1,20 @@
-# image_url = 'https://ssl.pstatic.net/imgfinance/chart/item/area/day/195990.png?sidcode=1608439379118'
-#image_url으로부터 content 얻음
-# img_data = requests.get(image_url).content
-# os.chdir(r'####경로를 정확히 입력 #####')
-# #content 저장함
-# with open('image_name.jpg', 'wb') as handler:
-#     handler.write(img_data)
-
+#기본: 네이버 웹툰 순위 예쁘게 크롤링하기 
 from bs4 import BeautifulSoup as bs
 import requests,re
 url = ('https://comic.naver.com/webtoon/weekday.nhn')
 headers = {"User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36'}
 res = requests.get(url, headers = headers)
 soup = bs(res.text, 'lxml')
-# ol만 찾아서 text를 찾을 경우0
-# for i in soup.find_all('ol', {'id':'realTimeRankUpdate'}):
-#     print(i.text)
-for i in soup.find_all('ol', {'id':'realTimeRankFavorite'}):
-    print([j.get('title') for j in i.find_all('a')])
+
+# ol만 찾아서 text를 찾을 경우, 성공 그러나 지저분함
+for i in soup.find_all('ol', {'id':'realTimeRankUpdate'}):
+    print(i.text)
+
+# 10대 실시간 인기만화(남자) 타이틀만 빼기 성공
 for i in soup.find_all('div', {'class':'thumb6'}):
     print(i.find('a').get('title'))
+
+# 인기 만화 리스트로 반환하기 성공
+for i in soup.find_all('ol', {'id':'realTimeRankFavorite'}):
+    print([j.get('title') for j in i.find_all('a')])
+
